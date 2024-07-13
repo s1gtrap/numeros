b7e55a12e564331a6bb59967ad3deb50d3329e3c
William Tange <will@tan.ge>
2024-07-13 18:13:02 +0200

    Force CSR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import "@/styles/globals.css";
+
+const App = dynamic(() => import("../components/App"), { ssr: false });
+
+export default (props: AppProps) => <App {...props} />;
