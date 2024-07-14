8c9f3a1579d87ae541547b64029aaa037ff6c2d8
William Tange <will@tan.ge>
2024-07-14 09:21:09 +0200

    Core game mechanics
    Generate correct answer, display it with words, reset on correct guess

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,61 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import React, { useEffect, useState } from "react";
+import { ToWords } from "to-words";
 
 const inter = Inter({ subsets: ["latin"] });
 
+function random(min: number, max: number): number {
+  return Math.floor(Math.random() * max + min);
+}
+
 export default function Home() {
+  const min = 0;
+  const max = 100;
+
+  const toWords = new ToWords({
+    localeCode: "en-US",
+  });
+
+  const [answer, setAnswer] = useState(random(min, max));
+  const [guess, setGuess] = useState("");
+
+  useEffect(() => {
+    function handleKeyUp(e: KeyboardEvent) {
+      if (48 <= e.keyCode && e.keyCode <= 57) {
+        setGuess(`${guess}${e.key}`);
+      } else if (e.keyCode === 8) {
+        setGuess(guess.slice(0, guess.length - 1));
+      } else if (e.keyCode === 13) {
+        if (guess === answer.toString()) {
+          setAnswer(random(min, max));
+          setGuess("");
+        }
+      }
+    }
+
+    document.body.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.body.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [guess]);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex flex-col h-screen items-center justify-between ${inter.className}`}
     >
-      hello world
+      <div className="flex grow items-center">
+        <p className="drop-shadow-md hyphens-auto text-4xl text-center text-rose-red">
+          {toWords.convert(answer)}
+        </p>
+      </div>
+      <div className="flex grow items-center">
+        <p
+          className={`drop-shadow-md text-4xl text-cerulean ${guess ? "" : "font-thin opacity-50"}`}
+        >
+          {guess || "Type the digits here!"}
+        </p>
+      </div>
     </main>
   );
 }
