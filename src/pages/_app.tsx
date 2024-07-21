7a47d07ee2694de2367cee9ce8e09c568f3adbc8
William Tange <will@tan.ge>
2024-07-21 11:18:34 +0200

    Fix file casing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,6 @@ import dynamic from "next/dynamic";
 
 import "@/styles/globals.css";
 
-const App = dynamic(() => import("../components/App"), { ssr: false });
+const App = dynamic(() => import("../components/app"), { ssr: false });
 
 export default (props: AppProps) => <App {...props} />;
